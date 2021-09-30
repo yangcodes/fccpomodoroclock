2509aba5d809942a7bff81e3bc32579d7075b5d7
Jenny Lin <misslinyang91@gmail.com>
2021-09-30 15:34:39 +1000

    created a pomodoro clock project using project

---
+++ src/components/SessionLength.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function SessionLength(props) {
+  function increaseSession() {
+    if (props.sessionLength === 60) {
+      return;
+    }
+    props.increaseSession();
+  }
+  function decreaseSession() {
+    if (props.sessionLength === 1) {
+      return;
+    }
+    props.decreaseSession();
+  }
+
+  return (
+    <section>
+      <h4>Session Length</h4>
+      <section className="interval-container">
+        <button
+          disabled={props.isPlay === true ? "disabled" : ""}
+          onClick={decreaseSession}
+        >
+          Down
+        </button>
+        <p className="interval-length">{props.sessionLength}</p>
+        <button
+          disabled={props.isPlay === true ? "disabled" : ""}
+          onClick={increaseSession}
+        >
+          Up
+        </button>
+      </section>
+    </section>
+  );
+}
+
+export default SessionLength;
